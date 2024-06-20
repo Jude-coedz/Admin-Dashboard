844cd0a1307ccf885f757eaa0e06cab7bab0bd5b
Jude-Akede <ee.akede@gmail.com>
2024-06-20 11:56:42 +0100

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,38 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '86631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'LARVENDER KF182 Drone',
+        productNumber: '36721',
+        paymentStatus: 'Refunded',
+        shipping:'Declined'
+    },
+    {
+        productName: 'Ruko F11 Pro Drone',
+        productNumber: '96996',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'GPS 4k Drone',
+        productNumber: '22821',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'DJI Air 25',
+        productNumber: '81475',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Lozenge Drone',
+        productNumber: '93214',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    }
+]
